48615b89d90bde3e3fde8e67be6520b8db8c9a2a
Lucas Amaral <koalasoftbr@gmail.com>
2020-09-05 14:56:12 -0300

    ajuste final

---
+++ php/port.php
@@ -0,0 +1,83 @@
+<div class="unslate_co--section" id="portfolio-section">
+        <div class="container">
+          
+          <div class="relative"><div class="loader-portfolio-wrap"><div class="loader-portfolio dark"></div></div> </div>
+          <div id="portfolio-single-holder"></div>
+
+          <div class="portfolio-wrapper">
+
+            <div class="d-flex align-items-center mb-4 gsap-reveal gsap-reveal-filter">
+              <h2 class="mr-auto heading-h2">Portfolio</h2>              
+            </div>
+
+            <div id="posts" class="row gutter-isotope-item">
+
+              <div class="item web packaging col-sm-6 col-md-6 col-lg-4 isotope-mb-2">
+                <a href="images/work_1.png" class="portfolio-item isotope-item gsap-reveal-img" data-fancybox="gallery" data-caption="Watch">
+                    <div class="overlay">
+                      <span class="wrap-icon icon-photo2"></span>
+                      <div class="portfolio-item-content">
+                        <h3></h3>
+                        <p></p>
+                      </div>
+                    </div>
+                    <img src="images/work_1.png" class="lazyload  img-fluid" alt="Images" />
+                  </a>
+              </div>
+
+              <div class="item web packaging col-sm-6 col-md-6 col-lg-4 isotope-mb-2">
+                <a href="images/work_2.png" class="portfolio-item isotope-item gsap-reveal-img" data-fancybox="gallery" data-caption="Watch">
+                    <div class="overlay">
+                      <span class="wrap-icon icon-photo2"></span>
+                      <div class="portfolio-item-content">
+                        <h3></h3>
+                        <p></p>
+                      </div>
+                    </div>
+                    <img src="images/work_2.png" class="lazyload  img-fluid" alt="Images" />
+                  </a>
+              </div>
+
+              <div class="item web packaging col-sm-6 col-md-6 col-lg-4 isotope-mb-2">
+                <a href="images/work_3.png" class="portfolio-item isotope-item gsap-reveal-img" data-fancybox="gallery" data-caption="Watch">
+                    <div class="overlay">
+                      <span class="wrap-icon icon-photo2"></span>
+                      <div class="portfolio-item-content">
+                        <h3></h3>
+                        <p></p>
+                      </div>
+                    </div>
+                    <img src="images/work_3.png" class="lazyload  img-fluid" alt="Images" />
+                  </a>
+              </div>
+
+              <div class="item web packaging col-sm-6 col-md-6 col-lg-4 isotope-mb-2">
+                <a href="images/work_4.jpg" class="portfolio-item isotope-item gsap-reveal-img" data-fancybox="gallery" data-caption="Watch">
+                    <div class="overlay">
+                      <span class="wrap-icon icon-photo2"></span>
+                      <div class="portfolio-item-content">
+                        <h3></h3>
+                        <p></p>
+                      </div>
+                    </div>
+                    <img src="images/work_4.jpg" class="lazyload  img-fluid" alt="Images" />
+                  </a>
+              </div>
+
+              <div class="item web packaging col-sm-6 col-md-6 col-lg-4 isotope-mb-2">
+                <a href="images/work_5.png" class="portfolio-item isotope-item gsap-reveal-img" data-fancybox="gallery" data-caption="Watch">
+                    <div class="overlay">
+                      <span class="wrap-icon icon-photo2"></span>
+                      <div class="portfolio-item-content">
+                        <h3></h3>
+                        <p></p>
+                      </div>
+                    </div>
+                    <img src="images/work_5.png" class="lazyload  img-fluid" alt="Images" />
+                  </a>
+              </div>
+  
+            </div>
+          </div>
+        </div>
+      </div>
